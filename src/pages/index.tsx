a3d50e3631214b749d769f1928fc64dc2f2046d3
= Moisés Carvalho <moisescarv27@hotmail.com>
2023-09-13 19:51:03 -0300

    Add Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,20 @@
+import NavButton from 'components/NavButton'
+import Section from 'components/Section'
+
 export default function Home() {
-  return <main></main>
+  return (
+    <>
+      <h1 className="text-3xl font-semibold">CS Matches</h1>
+
+      <Section title="Dashboard (TODO!)"></Section>
+
+      <Section title="Create new">
+        <nav className="flex gap-3">
+          <NavButton title="Team" destination="/team/create" />
+          <NavButton title="Player" destination="/player/create" />
+          <NavButton title="Match" destination="/match/create" />
+        </nav>
+      </Section>
+    </>
+  )
 }
