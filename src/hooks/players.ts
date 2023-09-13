80d93f10c5067e4d839fcc68e4f9928fc19915f2
= Moisés Carvalho <moisescarv27@hotmail.com>
2023-09-13 19:50:44 -0300

    Create hooks for test data

---
+++ src/hooks/players.ts
@@ -0,0 +1,9 @@
+export function usePlayers() {
+  return {
+    players: [
+      { id: 1, name: 'Jorginho da Silva' },
+      { id: 2, name: 'Lucas Hardskate' },
+      { id: 3, name: 'Biscoitão' },
+    ],
+  }
+}
---
+++ src/hooks/teams.ts
@@ -0,0 +1,9 @@
+export function useTeams() {
+  return {
+    teams: [
+      { id: 1, name: 'Maracujá FC' },
+      { id: 2, name: 'Winx' },
+      { id: 3, name: 'Batman' },
+    ],
+  }
+}
