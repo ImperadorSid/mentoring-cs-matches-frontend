1162d42562acfccfad6781c561d23e36afddb42d
= Moisés Carvalho <moisescarv27@hotmail.com>
2023-09-15 15:36:05 -0300

    Remove unused hooks

--- src/hooks/players.ts
+++
@@ -1,9 +0,0 @@
-export function usePlayers() {
-  return {
-    players: [
-      { id: 1, name: 'Jorginho da Silva' },
-      { id: 2, name: 'Lucas Hardskate' },
-      { id: 3, name: 'Biscoitão' },
-    ],
-  }
-}
--- src/hooks/teams.ts
+++
@@ -1,9 +0,0 @@
-export function useTeams() {
-  return {
-    teams: [
-      { id: 1, name: 'Maracujá FC' },
-      { id: 2, name: 'Winx' },
-      { id: 3, name: 'Batman' },
-    ],
-  }
-}
